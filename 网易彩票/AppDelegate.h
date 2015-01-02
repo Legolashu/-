2b683b1d04b7c1da911c6c6cd707cb276db38d44
yosemite <yosemite@yosemitedeMacBook-Pro.local>
2015-01-02 18:13:27 +0000

    Initial Commit

---
+++ 网易彩票/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  网易彩票
+//
+//  Created by yosemite on 15/1/2.
+//  Copyright (c) 2015年 heima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 网易彩票/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  网易彩票
+//
+//  Created by yosemite on 15/1/2.
+//  Copyright (c) 2015年 heima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
